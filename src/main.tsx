ca3f76418f43ba97ec730bcb2f0e41ef3827b955
foxxx4555 <rightwater156@gmail.com>
2026-02-15 21:39:33 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,3 +4,12 @@ import App from "./App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').then(reg => {
+      console.log('✅ تم تفعيل نظام العمل في الخلفية', reg);
+    }).catch(err => {
+      console.log('❌ فشل تفعيل نظام الخلفية', err);
+    });
+  });
+}
